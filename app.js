b62422cd056d425ad1dc24519f518aa9c87ba275
kirill <sotnikov_k@outlook.com>
2020-02-21 17:06:43 +0200

    deploy pwa

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(registration => {
+        console.log('Service worker registered', registration);
+      })
+      .catch(err => {
+        console.log('Service worker registration failed', error);
+      })
+  }
+
+})
